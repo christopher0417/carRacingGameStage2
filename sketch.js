0b72c2fc254c0dd36e9bfccd8c334f8c301b0b50
christopher <c.jabudavila@gmail.com>
2020-10-17 21:50:04 -0400

    game camera

--- sketch.js
+++ sketch.js
@@ -2,14 +2,16 @@ var canvas,backgroundImage;
 
 var gameState = 0;
 var playerCount;
+var allPlayers;
 
 var database;
 
 var form,player,game;
+var car1,car2,car3,car4,cars;
 
 function setup() {
 
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth-20,displayHeight-30);
   database = firebase.database();
   game = new Game();
   game.getState();
@@ -18,6 +20,15 @@ function setup() {
 }
 
 function draw() {
+
+  if(playerCount === 4) {
+    game.update(1);
+  }
+
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
 
 
 
